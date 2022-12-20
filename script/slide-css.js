7d628828d109d83df8db2d961b776d2fbfb22623
Super User <super.snailmail404@gmail.com>
2022-12-20 09:18:06 +0700

    Nav Fix

--- script/slide-css.js
+++ script/slide-css.js
@@ -4,3 +4,14 @@ const nav = document.querySelector('nav ul');
 menu.addEventListener('click', function() {
     nav.classList.toggle('slide');
 });
+
+
+// var docWidth = document.documentElement.offsetWidth;
+// [].forEach.call(
+//   document.querySelectorAll('*'),
+//   function(el) {
+//     if (el.offsetWidth > docWidth) {
+//       console.log(el);
+//     }
+//   }
+// );
