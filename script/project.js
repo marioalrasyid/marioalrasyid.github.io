d00553a2ef9141430679f2bd3f7e5c125c835cff
Super User <super.snailmail404@gmail.com>
2023-01-29 23:35:00 +0700

    Project Detail Update

---
+++ script/project.js
@@ -0,0 +1,19 @@
+
+// CARD 1
+document.getElementById("details-1").addEventListener("click", function() {
+  document.getElementById("detailCard-1").style.display = "block";
+});
+
+document.getElementById("close-1").addEventListener("click", function() {
+  document.getElementById("detailCard-1").style.display = "none";
+});
+
+// CARD 2
+document.getElementById("details-2").addEventListener("click", function() {
+  document.getElementById("detailCard-2").style.display = "block";
+});
+
+document.getElementById("close-2").addEventListener("click", function() {
+  document.getElementById("detailCard-2").style.display = "none";
+});
+
